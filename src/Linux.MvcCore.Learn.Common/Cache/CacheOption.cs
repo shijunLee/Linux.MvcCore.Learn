19f9f7f3a9ab8d6c361d7d366f30e6ad4f634c15
shijunLee <lishjun01@126.com>
2016-06-01 14:41:19 +0800

    add memory cache interface

---
+++ src/Linux.MvcCore.Learn.Common/Cache/CacheOption.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Linux.MvcCore.Learn.Common.Cache
+{
+    public class CacheOption : IOptions<MemoryCacheOptions>
+    {
+        public MemoryCacheOptions Value
+        {
+            get
+            {
+                MemoryCacheOptions options = new MemoryCacheOptions();
+               
+                return options;
+            }
+        }
+    }
+}
